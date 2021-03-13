fbcd89c57638316c246159924aac4dd9385322af
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-13 05:33:31 -0300

    Alteração do código

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace Arte
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.WriteLine("Tecle ENTER para ver a Arciiart");
             Console.ForegroundColor=ConsoleColor.DarkRed;
-            Console.WriteLine(  "||  ||");
-            Console.WriteLine(  "\\\\()// ");
+            Console.ReadKey();
+            Console.WriteLine(" ||  ||");
+            Console.ReadKey();
+            Console.WriteLine(" \\\\()//   ");
+            Console.ReadKey();
             Console.WriteLine("//(__)\\\\");
+            Console.ReadKey();
             Console.WriteLine("||    ||"); 
             Console.ResetColor();   
         }
