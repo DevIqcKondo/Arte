f3b56a1b098037132cfefff666e3d441dc8c902f
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-13 04:00:20 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine(  "||  ||");
+            Console.WriteLine(  "\\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||"); 
+            Console.ResetColor();   
+        }
+    }
+}
